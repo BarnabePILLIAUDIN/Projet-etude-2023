40312b50b7cca2a1839c0b0f0f60d9221956dc8f
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2023-04-12 13:41:49 +0200

    chore: initial files commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Hello from "@/components/Hello"
+import Page from "@/components/Page"
+
+
+
+export default function Home() {
+  return (
+    <Page>
+      <Hello></Hello>
+    </Page>
+  )
+}
