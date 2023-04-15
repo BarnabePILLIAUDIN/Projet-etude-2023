8e068be0fc1d45e227f8d8f096cc1a541aa1ed0e
BarnabePILLIAUDIN <barnabe.pilliaudin@gmail.com>
2023-04-15 15:09:48 +0200

    upadted last commit

--- src/pages/manageFacilities.jsx
+++ src/pages/manageFacilities.jsx
@@ -2,15 +2,40 @@ import Page from "@/components/Page"
 import ListFacility from "@/components/facilities/ListFacility"
 import { ClipboardDocumentListIcon } from "@heroicons/react/24/solid"
 import Link from "next/link"
-import React from "react"
+import { useRouter } from "next/router"
+import React, { useEffect, useState } from "react"
 
 const ManageFacilities = () => {
+  const [isLoged, setIsLoged] = useState(false) //eslint-disable-line
+  const [isAdmin, setIsAdmin] = useState(false)
+  const router = useRouter("/")
+
+  useEffect(() => {
+    const token = localStorage.getItem("easyRoomJWT")
+
+    if (token) {
+      setIsLoged(true)
+      const cleanToken = jwt.decode(token)
+      setIsAdmin(cleanToken.isAdmin)
+
+      return
+    }
+
+    router.push("/signin")
+  }, [])
+
   return (
     <Page title="EasyRooms: Manage facilities">
-      <ListFacility />
-      <Link href="/createFacility">
-        <ClipboardDocumentListIcon className="h-32 w-32 text-white bg-black rounded-full p-5 right-10 bottom-10 hover:scale-110 hover:drop-shadow-hover duration-700 fixed" />
-      </Link>
+      {isAdmin ? (
+        <>
+          <ListFacility />
+          <Link href="/createFacility">
+            <ClipboardDocumentListIcon className="h-32 w-32 text-white bg-black rounded-full p-5 right-10 bottom-10 hover:scale-110 hover:drop-shadow-hover duration-700 fixed" />
+          </Link>
+        </>
+      ) : (
+        <></>
+      )}
     </Page>
   )
 }
--- src/pages/myAccount.jsx
+++ src/pages/myAccount.jsx
@@ -85,7 +85,6 @@ const MyAccount = () => {
         </>
       ) : (
         <>
-          {" "}
           <button
             onClick={() => {
               setReseting(true)
